3f5fa9c57f0dad903b831a529fc80c8ddb3bddcc
noah victoriano <noah.victoriano@cvsu.edu.ph>
2026-01-02 10:05:38 +0800

    chore: syntax error due to vscode merge conflict editor

--- src/main.cpp
+++ src/main.cpp
@@ -11,44 +11,40 @@ int main(int argc, char* argv[]) {
   if (argc < 2 || argc > 3) {
     std::cerr << "Usage: " << argv[0] << " <fileName> [displayData]\n";
     return 1;
-    if (argc < 2 || argc > 3) {
-      std::cerr << "Usage: " << argv[0] << " <fileName> [displayData]\n";
-      return 1;
-    }
+  }
 
-    std::ifstream inputFile(argv[1]);
-    std::ifstream inputFile(argv[1]);
+  std::ifstream inputFile(argv[1]);
 
+  if (!inputFile.is_open()) {
+    std::cerr << "Error in opening file " << argv[1] << "\n";
+    return 1;
     if (!inputFile.is_open()) {
       std::cerr << "Error in opening file " << argv[1] << "\n";
       return 1;
-      if (!inputFile.is_open()) {
-        std::cerr << "Error in opening file " << argv[1] << "\n";
-        return 1;
-      }
-
-      if (argc == 3) displayData = argv[2];
-
-      std::vector<Token> tokens;
-
-      // Perform lexical analysis
-      lexer(inputFile, tokens);
-
-      inputFile.close();
-
-      tokenSize = tokens.size();
-
-      displayTokens(tokens);
-
-      if (tokenSize > 0 && tokens[tokenSize - 1].type == UNKNOWN) {
-        std::cout << "INVALID JSON\n";
-      } else {
-        if (!parser(tokens))
-          std::cout << "INVALID JSON\n";
-        else
-          std::cout << "VALID JSON\n";
-      }
-
-      return 0;
     }
+
+    if (argc == 3) displayData = argv[2];
+
+    std::vector<Token> tokens;
+
+    // Perform lexical analysis
+    lexer(inputFile, tokens);
+
+    inputFile.close();
+
+    tokenSize = tokens.size();
+
+    displayTokens(tokens);
+
+    if (tokenSize > 0 && tokens[tokenSize - 1].type == UNKNOWN) {
+      std::cout << "INVALID JSON\n";
+    } else {
+      if (!parser(tokens))
+        std::cout << "INVALID JSON\n";
+      else
+        std::cout << "VALID JSON\n";
+    }
+
+    return 0;
   }
+}
