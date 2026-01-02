8c152fd04d2bb57792429f777eef1cd44e7b4e33
noah victoriano <noah.victoriano@cvsu.edu.ph>
2026-01-02 10:16:35 +0800

    fuck this curly brace

--- src/main.cpp
+++ src/main.cpp
@@ -18,33 +18,32 @@ int main(int argc, char* argv[]) {
   if (!inputFile.is_open()) {
     std::cerr << "Error in opening file " << argv[1] << "\n";
     return 1;
-    if (!inputFile.is_open()) {
-      std::cerr << "Error in opening file " << argv[1] << "\n";
-      return 1;
-    }
-
-    if (argc == 3) displayData = argv[2];
-
-    std::vector<Token> tokens;
-
-    // Perform lexical analysis
-    lexer(inputFile, tokens);
-
-    inputFile.close();
-
-    tokenSize = tokens.size();
-
-    displayTokens(tokens);
-
-    if (tokenSize > 0 && tokens[tokenSize - 1].type == UNKNOWN) {
-      std::cout << "INVALID JSON\n";
-    } else {
-      if (!parser(tokens))
-        std::cout << "INVALID JSON\n";
-      else
-        std::cout << "VALID JSON\n";
-    }
-
-    return 0;
   }
+
+  // For displaying the tokens and parsing phase
+  if (argc == 3) displayData = argv[2];
+
+  std::vector<Token> tokens;
+
+  // Perform lexical analysis
+  lexer(inputFile, tokens);
+
+  inputFile.close();
+
+  tokenSize = tokens.size();
+
+  displayTokens(tokens);
+
+  // Check if invalid JSON found in lexical analysis
+  if (tokenSize > 0 && tokens[tokenSize - 1].type == UNKNOWN) {
+    std::cout << "INVALID JSON\n";
+  } else {
+    // perform parsing
+    if (!parser(tokens))
+      std::cout << "INVALID JSON\n";
+    else
+      std::cout << "VALID JSON\n";
+  }
+
+  return 0;
 }
